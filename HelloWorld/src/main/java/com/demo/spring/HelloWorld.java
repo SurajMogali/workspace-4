7ea65aa38e69e291c7940487b9187e81938c48b9
SurajMogali <mogalisuraj@yahoo.com>
2024-06-30 18:41:25 +0530

    second commit

--- HelloWorld/src/main/java/com/demo/spring/HelloWorld.java
+++ HelloWorld/src/main/java/com/demo/spring/HelloWorld.java
@@ -3,7 +3,7 @@ package com.demo.spring;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world,India Won Worldcup");
 
 	}
 
